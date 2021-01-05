dcf0d2b1bfc98101a30ce79fb114ae9047b5c86d
QuentinMELERO <Quentin.melero@hotmail.fr>
2021-01-05 09:15:01 +0100

    Q2 05/01

--- Main.py
+++ Main.py
@@ -6,7 +6,9 @@ Réalisé par Adrien Corsetti et Quentin Melero le 15/12/2020
 Interface graphique principale : Fait
 Insérer un alien qui bouge : Fait
 Insérer un vaisseau qui bouge avec les touches du clavier : Fait
-Permettre au joueur de tirer lorsqu’il appuie sur la touche espace du clavier : Entrain
+Permettre au joueur de tirer lorsqu’il appuie sur la touche espace du clavier : Fait
+Faire les blocs de protection : Entrain
+Faire en sorte que lorsque le missile et l'allien rentre en collison ils disparaissent : Entrain
 
 """
 
