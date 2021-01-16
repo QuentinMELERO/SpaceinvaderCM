2073f8e37621571ff6f33a0c96f93c606e1c4768
QuentinMELERO <Quentin.melero@hotmail.fr>
2021-01-16 22:27:51 +0100

    Derniere 2

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@
 Programme pour jeu du Space Invaders
 Réalisé par Adrien Corsetti et Quentin Melero 
 Programme jouable qui nécessite encore quelques améliorations : niveaux, alien spécial
-Lien git : https://github.com/QuentinMELERO/SpaceinvaderCM.git
+
 """ 
 
 # Importation des bibliothèques nécessaires 
